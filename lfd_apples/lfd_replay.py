c6000dcab69e78fcb1ea29fccaaad5eaff5a66f3
Alejo Velasquez <velasale@oregonstate.edu>
2025-10-14 15:11:29 -0700

    lfd_with_handle

--- lfd_apples/lfd_replay.py
+++ lfd_apples/lfd_replay.py
@@ -4,6 +4,13 @@ from rclpy.node import Node
 import pandas as pd
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from lfd_apples.ros2bag2csv import parse_array
+import time
+import numpy as np
+# from moveit.planning import MoveItPy
+
+print(np.pi)
+
+
 
 class FrankaReplay(Node):
     def __init__(self, csv_path):
@@ -63,11 +70,59 @@ class FrankaReplay(Node):
         self.publisher.publish(traj_msg)
         self.get_logger().info("Trajectory sent ✅")
 
+# class MoveToStart(Node):
+#     def __init__(self):
+#         super().__init__('move_to_start')
+#         self.publisher = self.create_publisher(JointTrajectory, '/fr3_arm_controller/joint_trajectory', 10)
+
+#     def go_to_position(self):
+
+#         group = MoveGroupCommander("panda_arm")
+#         group.set_joint_value_target([
+#                                         0.0,
+#                                         -np.pi/4,
+#                                         0.0,
+#                                         -3.0*np.pi/4,
+#                                         0.0,
+#                                         np.pi/2,
+#                                         np.pi/4
+#                                     ])
+
+
+#         # Optional: control speed/acceleration scaling
+#         group.set_max_velocity_scaling_factor(0.2)  # 20% speed
+#         group.set_max_acceleration_scaling_factor(0.2)
+
+#         # Plan and execute
+#         plan = group.plan()
+#         group.execute(plan[1])  # Execute planned trajectory
+#         self.get_logger().info("Sent robot to start position!")
+
+
+# def move_robot_to_start():
+#     rclpy.init()
+#     node = MoveToStart()
+
+#     # Give publisher time to connect
+#     time.sleep(1.0)
+
+#     node.go_to_position()
+
+#     # Allow time for execution
+#     time.sleep(6.0)
+
+#     node.destroy_node()
+#     rclpy.shutdown()
+
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init()
+    # node = MoveToStart()
 
-    lfd_demo_csv =  '/home/alejo/franka_bags/franka_joint_bag_1/bag_exports/NS_1_joint_states.csv'
+    # Give publisher time to connect
+    time.sleep(1.0)
+    
+    lfd_demo_csv =  '/home/alejo/franka_bags/franka_joint_bag_hdemo_2/bag_csvs/joint_states.csv'
 
     node = FrankaReplay(lfd_demo_csv)  # <-- set your CSV path
     rclpy.spin_once(node, timeout_sec=2.0)
--- lfd_apples/listen_franka.py
+++ lfd_apples/listen_franka.py
@@ -4,6 +4,10 @@ import time
 import signal
 import os
 
+import rclpy
+from rclpy.node import Node
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
 # Replace with your robot's IP and path to the compiled C++ Free Drive executable
 ROBOT_IP = "192.168.1.11"
 
@@ -12,8 +16,70 @@ BAG_DIR = os.path.expanduser("~/franka_bags")
 os.makedirs(BAG_DIR, exist_ok=True)
 BAG_NAME = os.path.join(BAG_DIR, "franka_joint_bag")
 
+# === Desired joint configuration ===
+JOINT_NAMES = [
+    "fr3_joint1",
+    "fr3_joint2",
+    "fr3_joint3",
+    "fr3_joint4",
+    "fr3_joint5",
+    "fr3_joint6",
+    "fr3_joint7"
+]
+
+JOINT_POSITIONS = [
+    1.4646141805298691,
+    -0.9702388178201579,
+    0.19573473944695943,
+    -2.6398837719728743,
+    0.19467791672497642,
+    1.6778097848819793,
+    0.7138629799943128,
+]
+
+class MoveToStart(Node):
+    def __init__(self):
+        super().__init__('move_to_start')
+        self.publisher = self.create_publisher(JointTrajectory, '/fr3_arm_controller/joint_trajectory', 10)
+
+    def go_to_position(self):
+        traj = JointTrajectory()
+        traj.joint_names = JOINT_NAMES
+
+        point = JointTrajectoryPoint()
+        point.positions = JOINT_POSITIONS
+        point.time_from_start = rclpy.duration.Duration(seconds=5.0).to_msg()  # 5s motion
+
+        traj.points.append(point)
+        self.publisher.publish(traj)
+        self.get_logger().info("Sent robot to start position!")
+
+
+def move_robot_to_start():
+    rclpy.init()
+    node = MoveToStart()
+
+    # Give publisher time to connect
+    time.sleep(1.0)
+
+    node.go_to_position()
+
+    # Allow time for execution
+    time.sleep(6.0)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+
 
 def main():
+
+    # === Step 1: Move robot to start ===
+    # print("Moving robot to start configuration...")
+    # move_robot_to_start()
+
+    # === Step 2: Start Free Drive mode ===
     # Start Free Drive in background
     input("Hit Enter to start Free Drive...")
 
