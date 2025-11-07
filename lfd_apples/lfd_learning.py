e77fcf5c7579b5ed31abef30db70e48bbc48952a
Alejo Velasquez <velasale@oregonstate.edu>
2025-11-06 17:21:31 -0800

    learning pipeline scripts

---
+++ lfd_apples/lfd_learning.py
@@ -0,0 +1,24 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+
+def main():
+
+    # Get data
+
+    # Split data into training and validation sets
+
+
+    # Initialize model
+    model = RandomForestClassifier()    
+    model.fit(X_train, y_train)
+
+    # Evaluate model on validation set
+
+
+
+
+
+if __name__ == '__main__':
+    main()
