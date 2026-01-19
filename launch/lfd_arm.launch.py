9713357f95b8ef0138c2a6cb32bc92aa06553542
Alejo Velasquez <velasale@oregonstate.edu>
2026-01-18 18:05:36 -0800

    servo node

--- launch/lfd_arm.launch.py
+++ launch/lfd_arm.launch.py
@@ -3,6 +3,13 @@ import yaml
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution
+)
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def load_yaml(package_name, file_path):
@@ -18,21 +25,36 @@ def load_yaml(package_name, file_path):
 
 
 def generate_launch_description():
+      
+    # Configurable parameters
+    robot_ip = '192.168.1.11'       #LaunchConfiguration('robot_ip')
+    use_fake_hardware = 'false'     #LaunchConfiguration('use_fake_hardware')
+    fake_sensor_commands = 'false'  #LaunchConfiguration('fake_sensor_commands')
+    namespace = ''                  #LaunchConfiguration('namespace')
+    load_gripper = 'true'           #LaunchConfiguration('load_gripper')
+    ee_id = 'franka_hand'           #LaunchConfiguration('ee_id')
+
+    # Planning_context
+    franka_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        'robots', 'fr3', 'fr3.urdf.xacro'
+    )
 
     # ====== MOVEIT 2  SERVO NODE ========
+
     # Get parameters for the servo node
     servo_yaml = load_yaml("lfd_apples", "config/lfd_servo.yaml")
     servo_params = {"moveit_servo": servo_yaml}
-
-    
-
+       
 
     servo_node = Node(
         package="moveit_servo",
         executable="servo_node_main",
         name="servo_node",
+        namespace=namespace,
         output="screen",
         parameters=[
+            robot_description,
             servo_params,
         ],
     )
