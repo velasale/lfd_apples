12a82bdf327c2c9519f021347ff5d03b8d10a843
Alejo Velasquez <velasale@oregonstate.edu>
2026-02-28 22:31:31 -0800

    allocated more cores for the camera

--- lfd_apples/resources/aliases.sh
+++ lfd_apples/resources/aliases.sh
@@ -8,10 +8,10 @@ alias suctionOff='ros2 service call /microROS/toggle_valve std_srvs/srv/SetBool 
 # taskset used to assign CPU cores
 alias arm='taskset -c 1-4 ros2 launch franka_fr3_moveit_config moveit.launch.py robot_ip:=192.168.1.11'
 alias human_demo='taskset -c 5-8 ros2 run lfd_apples lfd_data_collection'
-alias gripper='ros2 launch lfd_apples lfd_gripper.launch.py palm_camera_device_num:=6 fixed_camera_device_num:=4'
+alias gripper='taskset -c 5-8 ros2 launch lfd_apples lfd_gripper.launch.py palm_camera_device_num:=6 fixed_camera_device_num:=4'
 alias sensors='ros2 topic echo /microROS/sensor_data'
 alias lfd_layout='terminator -l lfd_trials'
 
-alias robot='taskset -c 5-8 ros2 run lfd_apples lfd_implementation'
+alias robot='taskset -c 9-12 ros2 run lfd_apples lfd_implementation'
 alias lfd_implementation_layout='terminator -l lfd_implement'
 alias reset_arm='ros2 run lfd_apples franka_recover'
